05d14f37e24c4c1eea283ccb5bf01ac3894e1c5e
hammer200911 <jdaniel231@gmail.com>
2022-10-22 11:03:05 -0300

    conexao bd

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import express from 'express'
+import { database } from './database'
 
 const app = express()
 
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, ()=> {
+  database.authenticate().then(()=> {
+    console.log('BD com sucesso!')
+  })
   console.log('Servidor com sucesso!')
 })
